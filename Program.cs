b09f19630d52fd619bb58a482e4ae2d1e204a926
Aleksey Burov <burovalexprogrammer@gmail.com>
2023-06-24 21:20:32 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+
+Console.Write("Введите число:");
+int n;
+int.TryParse(Console.ReadLine(), out n);
+Console.WriteLine(OurLibrary.OurClass.ToBinary(n));
+Console.WriteLine(OurLibrary.OurClass.ToOctal(n));
+Console.WriteLine(OurLibrary.OurClass.ToHex(n));
+
+
